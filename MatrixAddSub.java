6ca15982404c96ba03053b4b37d440ead1755df9
Jyoshna <jyoshna.maraboyina@gmail.com>
2025-01-28 21:43:54 +0530

    Add files via upload

---
+++ MatrixAddSub.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class MatrixAddSub {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int cols = scanner.nextInt();
+
+        int[][] matrix1 = new int[rows][cols];
+        int[][] matrix2 = new int[rows][cols];
+        int[][] sum = new int[rows][cols];
+        int[][] difference = new int[rows][cols];
+
+        System.out.println("Enter elements of the first matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Enter element [" + i + "][" + j + "]: ");
+                matrix1[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("Enter elements of the second matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Enter element [" + i + "][" + j + "]: ");
+                matrix2[i][j] = scanner.nextInt();
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sum[i][j] = matrix1[i][j] + matrix2[i][j];
+                difference[i][j] = matrix1[i][j] - matrix2[i][j];
+            }
+        }
+        System.out.println("Result of matrix addition:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Result of matrix subtraction:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(difference[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
